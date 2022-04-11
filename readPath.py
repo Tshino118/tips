b6b0dba9d6892ab188ce19d3aa4a7f484d10cd3c
Tshino118 <t.shinohara991108@gmail.com>
2022-04-11 12:15:23 +0900

    edit read path

--- readPath.py
+++ readPath.py
@@ -1,3 +1,23 @@
-import os
-import sys.argV
+import imp
+import sys
+import glob
+import csv
+
+def read_path(inputDir,outputDir):
+    filenames=[_ for _ in glob.iglob(f"{inputDir}/*")]
+    with open(f'{outputDir}/read_path.csv', 'w') as f:
+        writer = csv.writer(f)
+        writeRow=writer.writerow
+        [writeRow(i,path) for i,path in enumerate(filenames)]
+    return filenames
+
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    inDir=args[1]
+    print('input Folder:{arg[1]}')
+
+    outDir=args[2]
+    print('output Folder:{arg[2]}')
 
